c7ac1fd32e698aceff888a90ab56f93f684b4bf0
Ani Sargsyan <sargsyan.ani@betconstruct.com>
2021-10-18 11:18:28 +0400

    added Vehicle controller, view and created vehicle table

---
+++ src/Controller/VehicleController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Vehicle;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VehicleController extends AbstractController
+{
+    /**
+     * @Route("/vehicle", name="vehicle")
+     */
+    public function index(): Response
+    {
+        $vehicleTypes = $this->getDoctrine()
+            ->getRepository(Vehicle::class)
+            ->findAll();
+
+        return $this->render('vehicle/index.html.twig', [
+            'controller_name' => 'VehicleController',
+            'vehicleTypes' => $vehicleTypes,
+        ]);
+    }
+}
---
+++ src/Entity/Vehicle.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VehicleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VehicleRepository::class)
+ */
+class Vehicle
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $available_number;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAvailableNumber(): ?int
+    {
+        return $this->available_number;
+    }
+
+    public function setAvailableNumber(int $available_number): self
+    {
+        $this->available_number = $available_number;
+
+        return $this;
+    }
+}
